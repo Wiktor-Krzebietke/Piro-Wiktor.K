de28ce4e44ef63380db181a58fd7b3ffd8edf6e5
Wiktor-Krzebietke <156892319+Wiktor-Krzebietke@users.noreply.github.com>
2024-05-08 20:12:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+/*Burger-open - Global*/
 
 const burgerBtn = document.querySelector('.burger');
-const navList = document.querySelector('.nav-list')
+const navList = document.querySelector('.nav-list');
 
 burgerBtn.addEventListener('click', function(){
-    burgerBtn.classList.toggle('active')
-    navList.classList.toggle('active')
+    burgerBtn.classList.toggle('active');
+    navList.classList.toggle('active');
 }
 )
+
+/*Kopiowanie linków - Contactpage*/ 
+
+const fbCopy = document.querySelector("#fb_btn");
+
+fbCopy.addEventListener('click', function(){
+    const fbLink = document.querySelector('.link');
+    fbLink.select();
+    document.execCommand("copy");
+
+})
+
+const mailCopy = document.querySelector("#mail_btn");
+
+mailCopy.addEventListener('click', function(){
+    const mail = document.querySelector('.mail');
+    mail.select();
+    document.execCommand("copy");
+
+})
+
